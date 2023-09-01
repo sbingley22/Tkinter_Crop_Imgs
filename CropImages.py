f662b68d2217735531cfb23c6d71acc3d3675e26
Sean B <seanb4@hotmail.co.uk>
2023-09-01 13:43:48 +0100

    Added jpeg support

--- CropImages.py
+++ CropImages.py
@@ -3,7 +3,7 @@ from tkinter import filedialog, Text
 import os
 from PIL import Image
 
-pngs = []
+imgs = []
 cropTop = 0
 cropBottom = 0
 cropLeft = 0
@@ -13,19 +13,18 @@ root = tk.Tk()
 root.configure(background='black')
 root.title("Crop Images")
 
-
 def addImages():
     for widget in frame.winfo_children():
             widget.destroy()
-    filename = filedialog.askopenfilenames(title="Select PNGs",
-            filetypes=(("PNGs","*.png"), ("all files", "*.*")))
+    filename = filedialog.askopenfilenames(title="Select Images",
+            filetypes=(("Image files", "*.png *.jpg"), ("all files", "*.*")))
     
     lst = list(filename)
-    pngs.clear()
+    imgs.clear()
     for l in lst:
-            pngs.append(l)
-    for png in pngs:
-            label = tk.Label(frame, text=png, bg="grey")
+            imgs.append(l)
+    for img in imgs:
+            label = tk.Label(frame, text=img, bg="grey")
             label.pack()
 
 def CropImgs():
@@ -34,8 +33,8 @@ def CropImgs():
     cropLeft = cropLeftEntry.get()
     cropRight = cropRightEntry.get()
         
-    for png in pngs:
-        original = Image.open(png)
+    for img in imgs:
+        original = Image.open(img)
         orHeight = original.size[1]
         orWidth = original.size[0]
         newWidth = orWidth - (cropLeft + cropRight)
@@ -49,57 +48,60 @@ def CropImgs():
                 for y in range(newHeight):
                     newIm.putpixel((x,y), oldPix[x+cropLeft, y+cropTop])
                     
-            newPath = png.rsplit('/',1)[0] + '/Cropped/' + png.rsplit('/',1)[1]
-            directory = png.rsplit('/',1)[0] + '/Cropped'
+            newPath = img.rsplit('/',1)[0] + '/Cropped/' + img.rsplit('/',1)[1]
+            directory = img.rsplit('/',1)[0] + '/Cropped'
             if not os.path.exists(directory):
                 os.mkdir(directory)
 
+            #Test to see if image is jpg
+            if img.rsplit(".",1)[1] == "jpg" or "JPEG":
+                newIm = newIm.convert('RGB')
             newIm.save(newPath)
         else:
             print("Crop amount is bigger than image. Aborting.")
         
 
 
-canvas = tk.Canvas(root, height = 400, width = 400, bg="#263D42")
+canvas = tk.Canvas(root, height = 400, width = 400, bg="#262626")
 canvas.pack()
 
 frame = tk.Frame(root, bg="grey")
 frame.place(relwidth=.96,relheight=.3, relx=0.02, rely=0.02)
 
-frame2 = tk.Frame(root, bg="#263D42")
+frame2 = tk.Frame(root, bg="#262626")
 frame2.place(relwidth=.45,relheight=.6, relx=0.02, rely=0.35)
 
-frame3 = tk.Frame(root, bg="#263D42")
+frame3 = tk.Frame(root, bg="#262626")
 frame3.place(relwidth=.45,relheight=.6, relx=0.52, rely=0.35)
 
-CTLabel = tk.Label(frame2, text="Crop Top Pixels:", padx = 10, pady = 5, fg = "white", bg="#263D42")
+CTLabel = tk.Label(frame2, text="Crop Top Pixels:", padx = 10, pady = 5, fg = "white", bg="#262626")
 CTLabel.pack()
 cropTopEntry = tk.IntVar(value=100)
-CTEntry = tk.Entry(frame2, textvariable=cropTopEntry, fg = "white", bg="#263D42")
+CTEntry = tk.Entry(frame2, textvariable=cropTopEntry, fg = "white", bg="#262626")
 CTEntry.pack()
 
-CBLabel = tk.Label(frame2, text="Crop Bottom Pixels:", padx = 10, pady = 5, fg = "white", bg="#263D42")
+CBLabel = tk.Label(frame2, text="Crop Bottom Pixels:", padx = 10, pady = 5, fg = "white", bg="#262626")
 CBLabel.pack()
 cropBottomEntry = tk.IntVar(value=100)
-CBEntry = tk.Entry(frame2, textvariable=cropBottomEntry, fg = "white", bg="#263D42")
+CBEntry = tk.Entry(frame2, textvariable=cropBottomEntry, fg = "white", bg="#262626")
 CBEntry.pack()
 
-CLLabel = tk.Label(frame2, text="Crop Left Pixels:", padx = 10, pady = 5, fg = "white", bg="#263D42")
+CLLabel = tk.Label(frame2, text="Crop Left Pixels:", padx = 10, pady = 5, fg = "white", bg="#262626")
 CLLabel.pack()
 cropLeftEntry = tk.IntVar()
-CLEntry = tk.Entry(frame2, textvariable=cropLeftEntry, fg = "white", bg="#263D42")
+CLEntry = tk.Entry(frame2, textvariable=cropLeftEntry, fg = "white", bg="#262626")
 CLEntry.pack()
 
-CRLabel = tk.Label(frame2, text="Crop Right Pixels:", padx = 10, pady = 5, fg = "white", bg="#263D42")
+CRLabel = tk.Label(frame2, text="Crop Right Pixels:", padx = 10, pady = 5, fg = "white", bg="#262626")
 CRLabel.pack()
 cropRightEntry = tk.IntVar()
-CREntry = tk.Entry(frame2, textvariable=cropRightEntry, fg = "white", bg="#263D42")
+CREntry = tk.Entry(frame2, textvariable=cropRightEntry, fg = "white", bg="#262626")
 CREntry.pack()
 
-openFile = tk.Button(frame3, text="Open PNGs", padx = 10, pady = 5, fg = "white", bg="#263D42", command=addImages)
+openFile = tk.Button(frame3, text="Open Images", padx = 10, pady = 5, fg = "white", bg="#262626", command=addImages)
 openFile.pack()
 
-CropImg = tk.Button(frame3, text="Crop PNGs", padx = 10, pady = 5, fg = "white", bg="#263D42", command=CropImgs)
+CropImg = tk.Button(frame3, text="Crop Images", padx = 10, pady = 5, fg = "white", bg="#262626", command=CropImgs)
 CropImg.pack()
 
 root.mainloop()
